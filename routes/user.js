44d888d5fc9c00aeef3ffc7b33dceb386ccac605
khyinto <khyinto@naver.com>
2023-03-30 18:58:14 +0900

    라우터 2차 수정

--- routes/user.js
+++ routes/user.js
@@ -4,17 +4,16 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 const utils = require("../lib/utils");
 
+// '/users' API 라우터
+
 /* 전체 정보 가져오기 */
 router.get("/", async (req, res) => {
-  const { email, name } = JSON.parse(JSON.stringify(req.body));
-  console.log("email  : " + email + " , name : " + name);
-
   const users = await prisma.user.findMany();
-
-  const result = {
-    success: users,
-  };
-  utils.send(res, result);
+  if (users === null) {
+    utils.Error(res, "User not found");
+    return;
+  }
+  utils.Success(res, users);
 });
 
 /* 회원 가입 */
@@ -25,9 +24,9 @@ router.post("/join", async (req, res) => {
   const existingUser = await prisma.user.findUnique({ where: { email } });
 
   if (existingUser) {
-    return res
-      .status(409)
-      .json({ message: "User with this email already exists" });
+    //res.status(409).json({ message: "User with this email already exists" });
+    utils.Error(res, "User with this email already exists");
+    return;
   }
   const user = await prisma.user.create({
     data: {
@@ -41,6 +40,32 @@ router.post("/join", async (req, res) => {
       },
     },
   });
+  utils.Success(res, "Successfully created");
+});
+
+router.post("/post_title", async (req, res) => {
+  // 구조 분해
+  const { id, title } = JSON.parse(JSON.stringify(req.body));
+
+  try {
+    const users = await prisma.user.findUnique({ where: { id: id } });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const updatePost = await prisma.post.update({
+      where: { id },
+      data: { title: title },
+    });
+
+    if (updatePost === null) {
+      res.status(404).json({ message: "User not found" });
+      result;
+    }
+  } catch (err) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
   utils.send(res, result);
 });
 
