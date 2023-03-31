118585436066b35057e5d0a758dd0754096e7138
khyinto <khyinto@naver.com>
2023-03-31 18:11:48 +0900

    트랜젝션 예시 삽입

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -11,6 +11,7 @@ class User_Controller {
     this.router.post("/post_title", this.updatePostTile.bind(this));
   }
 
+  /* ------------------------------------------------------------------------------------- */
   // get all users
   async getAllUsers(req, res) {
     const users = await prisma.user.findMany();
@@ -20,59 +21,89 @@ class User_Controller {
     }
     utils.Success(res, users);
   }
+  /* ------------------------------------------------------------------------------------- */
 
+  /* ------------------------------------------------------------------------------------- */
   // create a new user
   async createUsers(req, res) {
     // 구조 분해
     const { email, name } = JSON.parse(JSON.stringify(req.body));
+    const result = await prisma
+      .$transaction(async (prisma) => {
+        const existingUser = await prisma.user
+          .findUnique({ where: { email } })
+          .catch((err) => {
+            utils.Error(res, "Error creating user");
+          });
 
-    const existingUser = await prisma.user.findUnique({ where: { email } });
+        if (existingUser) {
+          //utils.Error(res, "User with this email already exists");
+        }
 
-    if (existingUser) {
-      //res.status(409).json({ message: "User with this email already exists" });
-      utils.Error(res, "User with this email already exists");
-      return;
-    }
-    const user = await prisma.user.create({
-      data: {
-        name,
-        email,
-        posts: {
-          create: { title: "Hello World" },
-        },
-        profile: {
-          create: { bio: "I like turtles" },
-        },
-      },
-    });
-    utils.Success(res, "Successfully created");
+        const user = await prisma.user.create({
+          data: {
+            name,
+            email,
+            posts: {
+              create: { title: "Hello World" },
+            },
+            profile: {
+              create: { bio: "I like turtles" },
+            },
+          },
+        });
+        utils.Success(res, "Successfully created");
+      })
+      .catch((error) => {
+        utils.Error(res, "Error creating user");
+      });
   }
+  /* ------------------------------------------------------------------------------------- */
 
+  /* ------------------------------------------------------------------------------------- */
   // update post title
   async updatePostTile(req, res) {
     // 구조 분해
-    const { id, title } = JSON.parse(JSON.stringify(req.body));
+    // const { id, title } = JSON.parse(JSON.stringify(req.body));
 
-    //try {
-    const result = await prisma.user.findUnique({
-      where: { id: parseInt(id) },
-    });
-    if (result === null) {
-      utils.Error(res, "User with this");
-      return;
-    }
+    const groups = [
+      { id: parseInt(1), title: "HaHaHaHaHaHa" },
+      { id: parseInt(20), title: "HoHoHoHHoHHoH" },
+    ];
 
-    const updatePost = await prisma.post.update({
-      where: { id: parseInt(id) },
-      data: { title: title },
-    });
+    await prisma
+      .$transaction(async (prisma) => {
+        for (let data of groups) {
+          const _id = data.id;
+          const _title = data.title;
+          console.log(_id);
+          console.log(_title);
 
-    if (updatePost === null) {
-      utils.Error(res, "User not found");
-      return;
-    }
-    utils.send(res, "Successfully updated");
+          await prisma.user.findUnique({
+            where: { id: parseInt(_id) },
+          });
+          await prisma.post.update({
+            where: { id: parseInt(_id) },
+            data: { title: _title },
+          });
+        }
+        /*
+        await prisma.post.update(
+          {
+            where: { id: parseInt(id) },
+            data: { title: title },
+          }.catch((err) => {
+            utils.Error(res, "User not found");
+          })
+        );
+        */
+        utils.send(res, "Successfully updated");
+      })
+      .catch((err) => {
+        utils.send(res, "Error updated ");
+      });
   }
+  /* ------------------------------------------------------------------------------------- */
 }
 
 module.exports = User_Controller;
